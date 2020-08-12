4083a636249b215f570266596353462e9816c986
Sue Ye <sye@homeaway.com>
2020-08-11 23:31:53 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This repo predicts if a device reading is a failure or not, in order to decide the need for maintenance',
+    author='Sue Ye',
+    license='MIT',
+)
